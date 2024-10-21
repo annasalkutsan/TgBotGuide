f1749fd6159673179c4b035737c229deb0e7ba97
Anna Salkutsan <salkutsanann@gmail.com>
2024-10-21 13:39:49 +0300

    Repository

---
+++ TgBotGuide.Application/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace TgBotGuide.Application.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(Guid id);
+    Task<ICollection<T>> GetAllAsync();
+    Task<ICollection<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    Task AddRangeAsync(ICollection<T> entities);
+    void Update(T entity);
+    void Remove(T entity);
+    void RemoveRange(ICollection<T> entities);
+}
