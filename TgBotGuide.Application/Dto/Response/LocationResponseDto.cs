e248fe3aa92d35f520496cb5cdb3401f069965c0
Anna Salkutsan <salkutsanann@gmail.com>
2024-11-09 00:18:56 +0200

    Add Response DTOs

---
+++ TgBotGuide.Application/Dto/Response/LocationResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TgBotGuide.Application.Dto.Response;
+
+public class LocationResponseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Coordinates { get; set; }
+    public AddressDto Address { get; set; }
+    public string ImageUrl { get; set; }
+    public List<CategoryDto> Categories { get; set; }
+
+    public LocationResponseDto()
+    {
+        Categories = new List<CategoryDto>();
+    }
+}
