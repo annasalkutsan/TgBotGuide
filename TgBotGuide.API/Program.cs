4d48c068e20933e76f1bf683ca5b94895447a3cd
Anna Salkutsan <salkutsanann@gmail.com>
2024-11-11 22:24:04 +0200

    Dependency Injection

--- TgBotGuide.API/Program.cs
+++ TgBotGuide.API/Program.cs
@@ -1,13 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TgBotGuide.Application.Interfaces;
+using TgBotGuide.Application.Mapping;
+using TgBotGuide.Application.Services;
+using TgBotGuide.Domain.Entities;
+using TgBotGuide.Domain.Interfaces;
+using TgBotGuide.Infrastructure;
+using TgBotGuide.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddDbContext<TgBotGuideDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddScoped<IRepository<Category>, Repository<Category>>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+builder.Services.AddScoped<IRepository<City>, Repository<City>>();
+builder.Services.AddScoped<ICityService, CityService>();
+
+builder.Services.AddScoped<IRepository<Location>, Repository<Location>>();
+builder.Services.AddScoped<ILocationService, LocationService>();
+
+builder.Services.AddScoped<IRepository<LocationCategory>, Repository<LocationCategory>>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+//builder.Services.AddValidatorsFromAssemblyContaining<EmployeeRequestValidator>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +44,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
